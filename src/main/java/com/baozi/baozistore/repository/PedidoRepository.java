b29de6c79d50cf6e85527edc67d4d3c570c1168c
WalneiOliveira <woliveiraassis@gmail.com>
2026-04-19 15:46:05 -0300

    add classes do pacote repository ClienteRepository,PedidoRepository e ProdutoRepository

--- src/main/java/com/baozi/baozistore/repository/PedidoRepository.java
+++ src/main/java/com/baozi/baozistore/repository/PedidoRepository.java
@@ -1,4 +1,9 @@
 package com.baozi.baozistore.repository;
 
-public class PedidoRepository {
+import com.baozi.baozistore.model.Pedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PedidoRepository extends JpaRepository<Pedido, Long> {
 }
--- src/main/java/com/baozi/baozistore/repository/ProdutoRepository.java
+++ src/main/java/com/baozi/baozistore/repository/ProdutoRepository.java
@@ -1,4 +1,10 @@
 package com.baozi.baozistore.repository;
 
-public class ProdutoRepository {
+import com.baozi.baozistore.model.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+    // JPA fornece: save, findById, findAll, deleteById etc.
 }
